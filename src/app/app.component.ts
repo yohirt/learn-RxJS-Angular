bfa3ef90dca72806dacad8c226f1a3760a77c062
yohirt <rtaraszka@poczta.fm>
2022-04-02 15:39:16 +0200

    add fromEvents to button and link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,33 @@
-import { Component, VERSION } from '@angular/core';
-import { Observable, of, from } from 'rxjs';
+import { Component, ElementRef, VERSION, ViewChild } from '@angular/core';
+import { Observable, of, from, fromEvent } from 'rxjs';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'Angular :) ' + VERSION.major;
-  today: number = Date.now();
-  agentName: string;
-  nameOrder: string;
-
-  students: Observable<string[]> = of(['Mark', 'Ram', 'Sita', 'Lisa']);
-
-  studentObj = {
-    id: 10,
-    name: 'Ram',
-  };
-
-  $orders$: Observable<string> = from(['Fasion', 'Electronic', 'Mobile']);
-  // agents: Observable<string>;
-  student$: Observable<any> = of(this.studentObj);
+  // # fromEvents
+  @ViewChild('validate') validate: ElementRef;
+  @ViewChild('getLink') getLinkData: ElementRef;
 
   constructor() {}
-  ngOnInit(): void {
-    this.students.subscribe((data) => {
-      console.log(data);
-    });
-    this.student$.subscribe((data) => {
-      console.log(data);
-    });
 
-    this.$orders$.subscribe((data) => {
-      setInterval(() => {
-        this.nameOrder = data;
-      }, 2000);
+  ngOnInit(): void {}
+
+  rxJsEventObsevable() {
+    const btnObservable$ = fromEvent(this.validate?.nativeElement, 'click');
+    btnObservable$.subscribe((data) => {
+      console.log(data);
+    });
+  }
+
+  getEventObservable() {
+    const linkObservable$ = fromEvent(
+      this.getLinkData?.nativeElement,
+      'mouseover'
+    );
+    linkObservable$.subscribe((data) => {
+      console.log(data);
     });
   }
 }
