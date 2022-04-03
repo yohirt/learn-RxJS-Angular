9f62796298e4dd824088b9a2bc0c78cbbc33456e
yohirt <rtaraszka@poczta.fm>
2022-04-03 22:17:17 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,23 @@
 import { Component, ElementRef, VERSION, ViewChild } from '@angular/core';
-import { Observable, of, from, fromEvent } from 'rxjs';
+import { Observable, of, from, fromEvent, interval } from 'rxjs';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  // # fromEvents
-  @ViewChild('validate') validate: ElementRef;
-  @ViewChild('getLink') getLinkData: ElementRef;
-
+  arrOrd = ['Mobile', 'book', 'electronic', 'computers', 'AGD'];
+  orders$: Observable<string> = from(this.arrOrd);
   constructor() {}
 
-  ngOnInit(): void {}
-
-  rxJsEventObsevable() {
-    const btnObservable$ = fromEvent(this.validate?.nativeElement, 'click');
-    btnObservable$.subscribe((data) => {
-      console.log(data);
-    });
-  }
-
-  getEventObservable() {
-    const linkObservable$ = fromEvent(
-      this.getLinkData?.nativeElement,
-      'mouseover'
-    );
-    linkObservable$.subscribe((data) => {
-      console.log(data);
+  ngOnInit(): void {
+    this.orders$.subscribe((data) => {
+      const seqNum$ = interval(100);
+      seqNum$.subscribe((num) => {
+        if (num < 10) {
+          console.log(data + num);
+        }
+      });
     });
   }
 }
